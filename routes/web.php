0773f91d673c46eca644dcd784e4ba41b3747c5b
nickvehikl <n.whitford@vehikl.com>
2025-01-20 13:13:01 -0500

    create view all boxes page

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/boxes', function () {
+        return Inertia::render('Boxes/Index');
+    })->name('boxes.index');
 });
 
 require __DIR__.'/auth.php';
