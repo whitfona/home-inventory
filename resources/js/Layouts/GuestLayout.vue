861fa8f6ca3e2057d257a5069616108f45fcc698
nickvehikl <n.whitford@vehikl.com>
2025-01-20 13:47:15 -0500

    change logo

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -7,6 +7,8 @@ import { Link } from '@inertiajs/vue3';
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
     >
+        <ApplicationLogo class="h-16 w-16 fill-current text-gray-800" />
+
         <div
             class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
         >
