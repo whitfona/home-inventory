f912940c976a7fc8ff402174cb77e17129ceed26
nickvehikl <n.whitford@vehikl.com>
2025-01-20 09:10:56 -0500

    add route to search boxes and items

--- routes/web.php
+++ routes/web.php
@@ -28,13 +28,10 @@ Route::middleware('auth')->group(function () {
 
 Route::prefix('api')->group(function () {
     Route::get('/boxes', [BoxController::class, 'index']);
-    Route::get('/boxes/{box}', [BoxController::class, 'show']);
     Route::post('/boxes', [BoxController::class, 'store']);
     Route::put('/boxes/{box}', [BoxController::class, 'update']);
     Route::delete('/boxes/{box}', [BoxController::class, 'destroy']);
 
-    Route::get('/items', [ItemController::class, 'index']);
-    Route::get('/items/{item}', [ItemController::class, 'show']);
     Route::post('/items', [ItemController::class, 'store']);
     Route::put('/items/{item}', [ItemController::class, 'update']);
     Route::delete('/items/{item}', [ItemController::class, 'destroy']);
