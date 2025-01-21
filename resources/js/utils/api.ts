ec399e3deb0c151a2157d16c843968c3d1b6ee02
nickvehikl <n.whitford@vehikl.com>
2025-01-21 09:08:47 -0500

    create api utility

---
+++ resources/js/utils/api.ts
@@ -0,0 +1,27 @@
+const defaultHeaders = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+};
+
+export const api = {
+    get: (url: string) => fetch(url, {
+        headers: defaultHeaders
+    }),
+    
+    post: (url: string, data: any) => fetch(url, {
+        method: 'POST',
+        headers: defaultHeaders,
+        body: JSON.stringify(data)
+    }),
+    
+    put: (url: string, data: any) => fetch(url, {
+        method: 'PUT',
+        headers: defaultHeaders,
+        body: JSON.stringify(data)
+    }),
+    
+    delete: (url: string) => fetch(url, {
+        method: 'DELETE',
+        headers: defaultHeaders
+    })
+}; 
