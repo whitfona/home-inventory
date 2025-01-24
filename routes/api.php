5db7fa4536b309efe979b20ee09818b890b51aa5
nickvehikl <n.whitford@vehikl.com>
2025-01-24 09:16:57 -0500

    group routes

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,18 @@ use App\Http\Controllers\ItemController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/boxes', [BoxController::class, 'index']);
-Route::get('/boxes/{box}', [BoxController::class, 'show']);
-Route::post('/boxes', [BoxController::class, 'store']);
-Route::put('/boxes/{box}', [BoxController::class, 'update']);
-Route::delete('/boxes/{box}', [BoxController::class, 'destroy']);
+Route::prefix('boxes')->group(function () {
+    Route::get('/', [BoxController::class, 'index']);
+    Route::get('/{box}', [BoxController::class, 'show']);
+    Route::post('/', [BoxController::class, 'store']);
+    Route::put('/{box}', [BoxController::class, 'update']);
+    Route::delete('/{box}', [BoxController::class, 'destroy']);
 
-Route::get('/boxes/{box}/items', [ItemController::class, 'index']);
-Route::get('/boxes/{box}/items/{item}', [ItemController::class, 'show']);
-Route::post('/boxes/{box}/items', [ItemController::class, 'store']);
-Route::put('/boxes/{box}/items/{item}', [ItemController::class, 'update']);
-Route::delete('/boxes/{box}/items/{item}', [ItemController::class, 'destroy']);
+    Route::get('/{box}/items', [ItemController::class, 'index']);
+    Route::get('/{box}/items/{item}', [ItemController::class, 'show']);
+    Route::post('/{box}/items', [ItemController::class, 'store']);
+    Route::put('/{box}/items/{item}', [ItemController::class, 'update']);
+    Route::delete('/{box}/items/{item}', [ItemController::class, 'destroy']);
+});
 
 Route::get('/search', SearchController::class);
