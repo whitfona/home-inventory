7c71815fb08e91746d3a32a8bf17e09465cc8508
nickvehikl <n.whitford@vehikl.com>
2025-01-30 15:34:22 -0500

    add dark theme toggle

---
+++ resources/js/Components/DarkModeToggle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useToggle, useDark } from '@vueuse/core'
+import Sun from "@/Components/Icons/Sun.vue";
+import Moon from "@/Components/Icons/Moon.vue";
+
+const isDark = useDark({
+    selector: 'body',
+    valueDark: 'dark',
+    valueLight: 'light'
+})
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+    <button @click="toggleDark()" class="w-full px-4 py-2 flex gap-2 items-center text-sm transition duration-150 ease-in-out hover:bg-background-hover focus:bg-background-hover focus:outline-none">
+
+        <Sun v-if="isDark" size="1.5rem" stroke="primary" />
+        <Moon v-else size="1rem" fill="primary" />
+
+        <span class="text-primary">{{ isDark ? 'Light' : 'Dark' }}</span>
+    </button>
+</template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,6 +6,7 @@ import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
+import DarkModeToggle from "@/Components/DarkModeToggle.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -83,6 +84,7 @@ const showingNavigationDropdown = ref(false);
                                         >
                                             Log Out
                                         </DropdownLink>
+                                        <DarkModeToggle />
                                     </template>
                                 </Dropdown>
                             </div>
